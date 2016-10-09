ab4f3635640f9957e3bb35d2a4453f58ad8a7625
Nilay Krishna <nilay.krishna97@gmail.com>
2016-10-09 20:03:08 +0530

    Update life.m

--- life.m
+++ life.m
@@ -3,11 +3,13 @@
 %white= alive, black=dead
 
 len=500; 
-lattice=int8(rand(len,len)); %defining array of dimensions(len,len)
-up=[2:len 1]; down=[len 1:len-1]; 
-colormap(gray(2)); %refer to http://matlab.izmiran.ru/help/techdoc/ref/colormap.html
+lattice=int8(rand(len,len)); %defining array of dimensions(len,len) int8 converts into signed 8 bit integers
+up=[2:len 1]; 
+down=[len 1:len-1]; 
+colormap(bone(3)); %refer to http://matlab.izmiran.ru/help/techdoc/ref/colormap.html
 for i=1:100
     neighbours=lattice(up,:)+lattice(down,:)+lattice(:,up)+lattice(:,down)+lattice(up,up)+lattice(up,down)+lattice(down,up)+lattice(down,down);
     lattice = neighbours==3 | lattice & neighbours==2; %defining neighbourhood and setting conditions for being alive
-    image(lattice*2); pause(0.05) %generating a new image at each timestep of 0.05s
+    image(lattice*2); 
+    pause(0.05) %generating a new image at each timestep of 0.05s
 end

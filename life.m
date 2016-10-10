19344e25e2bbe770fbb7231dbe17927895d7272d
Nilay Krishna <nilay.krishna97@gmail.com>
2016-10-10 17:40:09 +0530

    Update life.m

--- life.m
+++ life.m
@@ -9,7 +9,7 @@ down=[len 1:len-1];
 colormap(bone(3)); %refer to http://matlab.izmiran.ru/help/techdoc/ref/colormap.html
 for i=1:100
     neighbours=lattice(up,:)+lattice(down,:)+lattice(:,up)+lattice(:,down)+lattice(up,up)+lattice(up,down)+lattice(down,up)+lattice(down,down);
-    lattice = neighbours==3 | lattice & neighbours==2; %defining neighbourhood and setting conditions for being alive
+    lattice = neighbours==3 | lattice & neighbours==2; %defining neighbourhood and setting conditions for being alive(|=or, &=and)
     image(lattice*2); 
     pause(0.05) %generating a new image at each timestep of 0.05s
 end
